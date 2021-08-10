13e4d6ca6cc2ec6f383d78813b7f25f2e729ab1e
Gayakantha Jayakody <gayanjayakody13@gmail.com>
2021-08-10 14:37:21 +0530

    Update script.py

--- Question 6/script.py
+++ Question 6/script.py
@@ -3,4 +3,4 @@ import sys
 
 salt="Km5d5ivMy8iexuHcZrsD"
 
-print((hashlib.pbkdf2_hmac('sha512', sys.argv[0].strip().encode(), salt.encode(), 200000)).hex())
+print((hashlib.pbkdf2_hmac('sha512', sys.argv[11].strip().encode(), salt.encode(), 200000)).hex())
